85c76c2cc7b88b27dccb613db5b9a4bfc747d19a
Ashwin Gonsalves <ashwingonsalves@gmail.com>
2017-01-16 15:54:56 +0530

    Final changes

--- local_test.rb
+++ local_test.rb
@@ -8,16 +8,15 @@ caps["browser_version"] = "54.0"
 caps["os"] = "Windows"
 caps["os_version"] = "10"
 caps["browserstack.debug"] = "true"
+caps["browserstack.local"] = ENV['BROWSERSTACK_LOCAL']
+caps["browserstack.localIdentifier"] = ENV['BROWSERSTACK_LOCAL_IDENTIFIER']
 caps["build"] = "Supp Travis RUN"
 caps["project"] = "Supp Travis RUN"
-caps["name"] = "Sample Test"
+caps["name"] = "Local Test"
 
 driver = Selenium::WebDriver.for(:remote,
   :url => "http://"+ENV['BROWSERSTACK_USER']+":"+ENV['BROWSERSTACK_ACCESS_KEY']+"@hub.browserstack.com/wd/hub",
   :desired_capabilities => caps)
-driver.navigate.to "http://www.google.com"
-element = driver.find_element(:name, 'q')
-element.send_keys "BrowserStack"
-element.submit
+driver.navigate.to "http://localhost"
 puts driver.title
 driver.quit
